17a76bd2282d97cc5da9f8bf5f38794a7b6d6fb3
ismailnyza <ismailnyzarsl@gmail.com>
2023-06-03 01:37:43 +0530

    part1

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const TodoItem = ({ title, completed, toggleTodo, removeTodo }) => {
+const TodoItem = ({
+  id,
+  title,
+  description,
+  label,
+  completed,
+  toggleTodo,
+  removeTodo,
+  time
+}) => {
   return (
     <div>
-      <input type="checkbox" checked={completed} onChange={toggleTodo} />
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => toggleTodo(id)}
+      />
       <span>{title}</span>
-      <button onClick={removeTodo}>Remove</button>
+      <span>{description}</span>
+      <span>{label}</span>
+      <span>{time}</span>
+      <button onClick={() => removeTodo(id)}>Remove</button>
     </div>
   );
 };
--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -4,6 +4,9 @@ import TodoItem from './TodoItem';
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodoTitle, setNewTodoTitle] = useState('');
+  const [newTodoDescription, setNewTodoDescription] = useState('');
+  const [newTodoLabel, setNewTodoLabel] = useState('');
+  const [newTodoTime, setNewTodoTime] = useState('');
 
   const addTodo = (e) => {
     e.preventDefault();
@@ -11,15 +14,21 @@ const TodoList = () => {
       const newTodo = {
         id: Date.now(),
         title: newTodoTitle,
+        description: newTodoDescription,
+        label: newTodoLabel,
+        time: newTodoTime,
         completed: false,
       };
       setTodos([...todos, newTodo]);
       setNewTodoTitle('');
+      setNewTodoDescription('');
+      setNewTodoLabel('');
+      setNewTodoTime('');
     }
   };
 
   const toggleTodo = (id) => {
-    const updatedTodos = todos.map(todo => {
+    const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
         return { ...todo, completed: !todo.completed };
       }
@@ -29,8 +38,25 @@ const TodoList = () => {
   };
 
   const removeTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
+  };
+
+  const sortByTime = () => {
+    const sortedTodos = [...todos].sort((a, b) => a.time.localeCompare(b.time));
+    setTodos(sortedTodos);
+  };
+
+  const sortByLabel = () => {
+    const sortedTodos = [...todos].sort((a, b) => a.label.localeCompare(b.label));
+    setTodos(sortedTodos);
+  };
+
+  const sortByDescription = () => {
+    const sortedTodos = [...todos].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+    setTodos(sortedTodos);
   };
 
   return (
@@ -42,16 +68,54 @@ const TodoList = () => {
           onChange={(e) => setNewTodoTitle(e.target.value)}
           placeholder="Enter a new todo"
         />
+        <input
+          type="text"
+          value={newTodoDescription}
+          onChange={(e) => setNewTodoDescription(e.target.value)}
+          placeholder="Enter the description"
+        />
+        <input
+          type="text"
+          value={newTodoLabel}
+          onChange={(e) => setNewTodoLabel(e.target.value)}
+          placeholder="Enter the label"
+        />
+        <br />
+        <input
+          type="time"
+          value={newTodoTime}
+          onChange={(e) => setNewTodoTime(e.target.value)}
+          placeholder="Enter the due time"
+        />
+
+        <br />
+        <button type="button" onClick={sortByTime}>
+          Sort by Time
+        </button>
+        <button type="button" onClick={sortByLabel}>
+          Sort by Label
+        </button>
+        <button type="button" onClick={sortByDescription}>
+          Sort by Description
+        </button>
+        <br />
+
         <button type="submit">Add Todo</button>
       </form>
-      {todos.map(todo => (
+      <p>Total Todos: {todos.length}</p>
+      {todos.map((todo) => (
         <TodoItem
-          key={todo.id}
-          title={todo.title}
-          completed={todo.completed}
-          toggleTodo={() => toggleTodo(todo.id)}
-          removeTodo={() => removeTodo(todo.id)}
-        />
+        key={todo.id}
+        id={todo.id}
+        title={todo.title}
+        description={todo.description}
+        label={todo.label}
+        time={todo.time}
+        completed={todo.completed}
+        toggleTodo={toggleTodo}
+        removeTodo={removeTodo}
+      />
+      
       ))}
     </div>
   );
